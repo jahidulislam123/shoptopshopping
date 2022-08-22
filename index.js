eb293531b1b97ece80fd5b73d78384e3cdfc9670
unknown <islam15-3879@diu.edu.bd>
2022-08-22 13:25:29 +0600

    added-api

--- index.js
+++ index.js
@@ -26,6 +26,14 @@ async function run() {
       const bookingCollection =client.db('shoptopshopping').collection('booking');
       const ordersCollection =client.db('shoptopshopping').collection('orders');
      
+     
+     
+      // admin post product
+      app.post('/productPost',async(req,res)=>{
+        const newProduct  =req.body;
+        const result =await productsCollection.insertOne(newProduct);
+        res.send(result);
+     })
     //  all products 
       app.get('/products',async(req,res)=>{
 
@@ -78,7 +86,72 @@ async function run() {
       const result =await productsCollection.deleteOne(query);
       res.send(result);
   })
+
+   
     
+  // user 
+  //users data post
+  app.post('/userPost',async(req,res)=>{
+    const newPost  =req.body;
+    const result =await usersCollection.insertOne(newPost);
+    res.send(result);
+ })
+
+  //users data get 
+  app.get('/users',async(req,res)=>{
+
+    const query ='';
+    const cursor =usersCollection.find(query);
+    const products =await cursor.toArray();
+    res.send(products);
+
+ })
+ //users delete 
+ app.delete('/users/:id',async(req,res)=>{
+  const id=req.params.id;
+  const query ={_id:ObjectID(id)}
+  const result =await usersCollection.deleteOne(query);
+  res.send(result);
+})
+
+  // orders API 
+
+  // order Post by users 
+  app.post('/orderPost',async(req,res)=>{
+    const orderPost  =req.body;
+    const result =await ordersCollection.insertOne(orderPost);
+    res.send(result);
+ })
+
+  //ALL ORDERS GETTING
+  
+  app.get('/orders',async(req,res)=>{
+
+    const query ='';
+    const cursor =ordersCollection.find(query);
+    const products =await cursor.toArray();
+    res.send(products);
+
+ })
+
+ //my orders getting
+ app.get('/order/:email',async(req,res)=>{
+  const email =req.params.email;
+  const query ={email:email};
+  const booking =await ordersCollection.find(query); 
+  const booking2 =await booking.toArray();
+  res.send(booking2);
+})
+
+//delete my  orders and delete  order by admin
+app.delete('/order/:id',async(req,res)=>{
+  const id=req.params.id;
+  const query ={_id:ObjectID(id)}
+  const result =await ordersCollection.deleteOne(query);
+  res.send(result);
+})
+
+
 
 
     } finally {
